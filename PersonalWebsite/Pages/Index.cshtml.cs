6f938e43bf552a7f8742dbb3db7df508bff31d19
xavie <xavie@XRLAP>
2022-12-03 20:01:59 -0500

    Add project files.

---
+++ PersonalWebsite/Pages/Index.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PersonalWebsite.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        public int Age { get; set; }
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+            
+        }
+
+        public void OnGet()
+        {
+            var birthYear = 2004;
+            var currentYear = DateTime.Today.Year;
+            Age = currentYear- birthYear;
+
+        }
+    }
+}
